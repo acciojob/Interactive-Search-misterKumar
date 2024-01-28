e6d69321b6be8ad7915212aeb837df5160efadb1
misterKumar <120304503+misterKumar@users.noreply.github.com>
2024-01-28 13:31:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+const btn = document.querySelector(".btn");
+const search = document.querySelector(".search");
+
+
+search.addEventListener("focus",()=>{
+	search.classList.add("active");
+})
